0bf99984c546b60fd2953d29bfe17eec64fb4cd8
Kevin I <kevin.ignatius@ui.ac.id>
2023-11-08 15:41:11 +0700

    +finishing again 2 tutorial 7\

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -141,6 +141,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                           );
                         },
                       );
+                      
                         _formKey.currentState!.reset();
                     }
                   
